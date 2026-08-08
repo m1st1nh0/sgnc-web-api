2c87bbbce669d5117e84fdcfe0bee6b1d6e33409
m1st1nh0 <lukaschamposki@gmail.com>
2026-08-07 22:32:34 -0300

    corrigindo url

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://sgnc-web-frontend.vercel.app/",
+        "https://sgnc-web-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
